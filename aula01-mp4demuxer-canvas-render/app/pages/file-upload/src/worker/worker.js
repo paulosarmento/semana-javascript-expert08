48ccc238314ddb34744f2b1613ca044d02219c95
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-10-26 12:28:31 -0300

    add videoProcessor

--- aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/worker/worker.js
+++ aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/worker/worker.js
@@ -1,8 +1,36 @@
-onmessage = ({ data }) => {
-  // console.log("recebido!!", data);
-  debugger;
-  setTimeout(() => {
-    self.postMessage({ status: "done" });
-  }, 2000);
-  // while (true) {}
+import VideoProcessor from "./videoProcessor.js";
+
+const qvgaConstraints = {
+  width: 320,
+  height: 240,
+};
+
+const vgaConstraints = {
+  width: 640,
+  height: 480,
+};
+const hdConstraints = { width: 1280, height: 720 };
+
+const encoderConfig = {
+  ...qvgaConstraints,
+  bitrate: 10e6,
+  // WebM
+  codec: "vp09.00.10.08",
+  pt: 4,
+  hardwareAcceleration: "prefer-software",
+  // MP4
+  //   codec: "avc1.42002A",
+  //   pt: 1,
+  //   hardwareAcceleration: "prefer-software",
+  //   avc: { format: "annexb" },
+};
+
+const videoProcessor = new VideoProcessor();
+
+onmessage = async ({ data }) => {
+  await videoProcessor.start({
+    file: data.file,
+    encoderConfig,
+  });
+  self.postMessage({ status: "done" });
 };
