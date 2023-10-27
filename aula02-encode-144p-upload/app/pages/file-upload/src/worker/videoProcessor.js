b41ba48ef89177eda5d04f15ab5a184250c09011
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-10-27 16:33:26 -0300

    encode144p

--- aula02-encode-144p-upload/app/pages/file-upload/src/worker/videoProcessor.js
+++ aula02-encode-144p-upload/app/pages/file-upload/src/worker/videoProcessor.js
@@ -12,7 +12,7 @@ export default class VideoProcessor {
   /**
    * @returns {ReadableStream}
    */
-  mp4Decoder(encoderConfig, stream) {
+  mp4Decoder(stream) {
     return new ReadableStream({
       start: async (controller) => {
         const decoder = new VideoDecoder({
@@ -29,25 +29,118 @@ export default class VideoProcessor {
           },
         });
 
-        return this.#mp4Demuxer
-          .run(stream, {
-            onConfig(config) {
-              decoder.configure(config);
-            },
-            /**
-             *
-             * @param {EncodedVideoChunk} chunk
-             */
-            onChunk(chunk) {
-              // debugger;
-              decoder.decode(chunk);
-            },
-          })
-          .then(() => {
-            setTimeout(() => {
-              controller.close();
-            }, 1000);
-          });
+        return this.#mp4Demuxer.run(stream, {
+          async onConfig(config) {
+            const { supported } = await VideoDecoder.isConfigSupported(config);
+            if (!supported) {
+              console.error(
+                "mp4Muxer VideoDecoder config not supported!",
+                config,
+                controller.close()
+              );
+              return;
+            }
+            decoder.configure(config);
+          },
+          /**
+           *
+           * @param {EncodedVideoChunk} chunk
+           */
+          onChunk(chunk) {
+            // debugger;
+            decoder.decode(chunk);
+          },
+        });
+        // .then(() => {
+        //   setTimeout(() => {
+        //     controller.close();
+        //   }, 1000);
+        // });
+      },
+    });
+  }
+
+  encode144p(encoderConfig) {
+    let _encoder;
+    const readable = new ReadableStream({
+      start: async (controller) => {
+        const { supported } = await VideoEncoder.isConfigSupported(
+          encoderConfig
+        );
+        if (!supported) {
+          const message = "encode144p VideoDecoder config not supported!";
+          console.error(message, encoderConfig);
+          controller.error(message);
+          return;
+        }
+
+        _encoder = new VideoEncoder({
+          /**
+           *
+           * @param {EncodedVideoChunk} frame
+           * @param {EncodedVideoChunkMetadata} config
+           */
+
+          output: (frame, config) => {
+            if (config.decoderConfig) {
+              const decoderConfig = {
+                type: "config",
+                config: config.decoderConfig,
+              };
+              controller.enqueue(decoderConfig);
+            }
+            controller.enqueue(frame);
+          },
+          error: (err) => {
+            console.error("VideoEncoder 144p", err);
+            controller.error(err);
+          },
+        });
+
+        await _encoder.configure(encoderConfig);
+      },
+    });
+
+    const writable = new WritableStream({
+      async write(frame) {
+        _encoder.encode(frame);
+        frame.close();
+      },
+    });
+    return {
+      readable,
+      writable,
+    };
+  }
+
+  renderDecodedFramesAndGetEncodedChunks(renderFrame) {
+    let _decoder;
+    return new TransformStream({
+      start: (controller) => {
+        _decoder = new VideoDecoder({
+          output(frame) {
+            renderFrame(frame);
+          },
+          error(e) {
+            console.error("error at renderFrames", e);
+            controller.error(e);
+          },
+        });
+      },
+      /**
+       *
+       * @param {EncodedVideoChunk} encodedChunk
+       * @param {TransformStreamDefaultController} controller
+       */
+      async transform(encodedChunk, controller) {
+        if (encodedChunk.type === "config") {
+          await _decoder.configure(encodedChunk.config);
+          return;
+        }
+        _decoder.decode(encodedChunk);
+
+        // need the encoded version to use webM
+        controller.enqueue(encodedChunk);
       },
     });
   }
@@ -55,12 +148,16 @@ export default class VideoProcessor {
   async start({ file, encoderConfig, renderFrame }) {
     const stream = file.stream();
     const fileName = file.name.split("/").pop().replace(".mp4", "");
-    await this.mp4Decoder(encoderConfig, stream).pipeTo(
-      new WritableStream({
-        write(frame) {
-          renderFrame(frame);
-        },
-      })
-    );
+    await this.mp4Decoder(stream)
+      .pipeThrough(this.encode144p(encoderConfig))
+      .pipeThrough(this.renderDecodedFramesAndGetEncodedChunks(renderFrame))
+      .pipeTo(
+        new WritableStream({
+          write(frame) {
+            // debugger;
+            // renderFrame(frame);
+          },
+        })
+      );
   }
 }
